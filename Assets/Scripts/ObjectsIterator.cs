ed9f748376d207c109002123a001125eb034e76c
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-08-09 14:35:52 -0300

    Some array exercises and optimizations

---
+++ Assets/Scripts/ObjectsIterator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectsIterator : MonoBehaviour
+{
+    //así se define un array
+    GameObject[] mesas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //el método FindGameObjectsWithTag devuelve un array, por eso no es necesario
+        //inicializarlo explícitamente
+        mesas = GameObject.FindGameObjectsWithTag("mesa");
+        Debug.Log(mesas.Length);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            DestroyObjectsInArray(mesas);
+        }
+    }
+
+    public void DestroyObjectsInArray(GameObject[] array)
+    {
+        for (int i = 0;i<array.Length;i++)
+        {
+            Destroy(array[i]);
+        }
+    }
+
+
+}
