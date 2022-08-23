586f5bff4be7ccafb37ddee1e4fb01d264bb4f8d
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-08-23 09:02:40 -0300

    First attempt

---
+++ Assets/RaycastInteraction.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastInteraction : MonoBehaviour
+{
+    public bool inRange = false;
+    public GameObject pickableInRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && inRange)
+        {
+            pickableInRange.SetActive(false);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 2f))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit " + hit.collider.gameObject.tag);
+            if (hit.collider.gameObject.CompareTag("pickable"))
+            {
+                inRange = true;
+                pickableInRange = hit.collider.gameObject;
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+            inRange = false;
+        }
+    }
+}
