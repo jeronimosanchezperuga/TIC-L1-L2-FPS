7822ad530cc716821c33ca969ea56fe65ebf248f
Gonzanapo <88498679+Gonzanapo@users.noreply.github.com>
2022-10-03 11:13:35 -0300

    .

---
+++ Assets/Scripts/AgentScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class AgentScript : MonoBehaviour
+{
+    public Transform destinationTransform;
+    public NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = destinationTransform.position;
+    
+    }
+}
